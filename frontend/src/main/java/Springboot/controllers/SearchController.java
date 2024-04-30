f05ec9b312f322d28030f912e313a06a9371d0a0
tanhinho <miguel1leopoldo@gmail.com>
2024-04-30 17:27:44 +0100

    Init frontend

---
+++ frontend/src/main/java/Springboot/controllers/SearchController.java
@@ -0,0 +1,19 @@
+package Springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:/search";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+
+}
