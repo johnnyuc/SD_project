7f04631bdbadc281f9debb9b7c70d30bd8ae9638
tanhinho <miguel1leopoldo@gmail.com>
2024-05-15 20:10:18 +0100

    More javadoc

--- frontend/src/main/java/Springboot/hackernews/HackerNewsItemRecord.java
+++ frontend/src/main/java/Springboot/hackernews/HackerNewsItemRecord.java
@@ -5,24 +5,82 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
- * Represents an item record retrieved from Hacker News API.
+ * Represents an item retrieved from the Hacker News API.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record HackerNewsItemRecord(
-        Integer id, // The item's unique id.
-        Boolean deleted, // true if the item is deleted.
-        String type, // The type of item. One of "job", "story", "comment", "poll", or "pollopt".
-        String by, // The username of the item's author.
-        Long time, // Creation date of the item, in Unix Time.
-        String text, // The comment, story, or poll text. HTML.
-        Boolean dead, // true if the item is dead.
-        String parent, // The comment's parent: either another comment or the relevant story.
-        Integer poll, // The pollopt's associated poll.
-        List<Integer> kids, // The ids of the item's comments, in ranked display order.
-        String url, // The URL of the story.
-        Integer score, // The story's score, or the votes for a pollopt.
-        String title, // The title of the story, poll, or job. HTML.
-        List<Integer> parts, // A list of related pollopts, in display order.
-        Integer descendants // In the case of stories or polls, the total comment count.
-) {
+        /**
+         * The item's unique id.
+         */
+        Integer id,
+
+        /**
+         * True if the item is deleted.
+         */
+        Boolean deleted,
+
+        /**
+         * The type of item: "job", "story", "comment", "poll", or "pollopt".
+         */
+        String type,
+
+        /**
+         * The username of the item's author.
+         */
+        String by,
+
+        /**
+         * Creation date of the item, in Unix Time.
+         */
+        Long time,
+
+        /**
+         * The comment, story, or poll text in HTML format.
+         */
+        String text,
+
+        /**
+         * True if the item is dead.
+         */
+        Boolean dead,
+
+        /**
+         * The comment's parent: either another comment or the relevant story.
+         */
+        String parent,
+
+        /**
+         * The pollopt's associated poll.
+         */
+        Integer poll,
+
+        /**
+         * The ids of the item's comments, in ranked display order.
+         */
+        List<Integer> kids,
+
+        /**
+         * The URL of the story.
+         */
+        String url,
+
+        /**
+         * The story's score or the votes for a pollopt.
+         */
+        Integer score,
+
+        /**
+         * The title of the story, poll, or job in HTML format.
+         */
+        String title,
+
+        /**
+         * A list of related pollopts in display order.
+         */
+        List<Integer> parts,
+
+        /**
+         * Total comment count for stories or polls.
+         */
+        Integer descendants) {
 }
--- frontend/src/main/java/Springboot/util/Message.java
+++ frontend/src/main/java/Springboot/util/Message.java
@@ -3,5 +3,9 @@ package Springboot.util;
 /**
  * Represents a message with content.
  */
-public record Message(String content) {
+public record Message(
+        /**
+         * The content of the message.
+         */
+        String content) {
 }
